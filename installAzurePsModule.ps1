91b0d0ebcfbfc13fb292500eb5fd0825a5151a48
shaunm-msft <2079544+shaunm-msft@users.noreply.github.com>
2019-03-05 11:51:30 -0800

    Create installAzurePsModule.ps1

---
+++ installAzurePsModule.ps1
@@ -0,0 +1,17 @@
+$outputPath = "$env:ProgramData\vmssInstall\AzurePsModuleInstall.log"
+$nl = [Environment]::NewLineNewLine
+[IO.File]::WriteAllText($outputPath, "Attempting to install Azure PowerShell modules.$nl")
+if (!(Test-Path z:\))
+{
+    $isInstalled = Get-Module -ListAvailable -Name AzureRm
+    [IO.File]::AppendAllText($outputPath, "$isInstalled = Get-Module -ListAvailable -Name AzureRm$nl")
+	if (!$isInstalled)
+	{
+		$psrepo = Get-PSRepository
+		[IO.File]::AppendAllText($outputPath, "$psrepo = Get-PSRepository$nl")
+
+		$installedModule = Install-Module -Name AzureRm -Force
+		[IO.File]::AppendAllText($outputPath, "$installedModule = Install-Module -Name AzureRm -Force$nl")
+	}
+}
+[IO.File]::AppendAllText($outputPath, "Done installing Azure PowerShell modules.$nl")
